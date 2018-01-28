647f9e64a9c1976bc04f906d4f1603ad8c8e52b4
Tif Flowers <tif@gentlebull.com>
2018-01-28 23:29:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var name = "Joe"
 var height = "74"
 var name = "Susan"
+var height = "70"
 
 if (name === "Susan") {
     console.log("The name is correct")
@@ -8,10 +9,10 @@ if (name === "Susan") {
     console.log("Expected the name to be Susan")
 }
 
-if (height === "74") {
-    console.log("The name is correct")
+if (height === "70") {
+    console.log("The height is correct")
 } else {
-    console.log("Expected the name to be Susan")
+    console.log("Expected the height to be 74")
 }
 // Don't worry about this
 module.exports = { name, height
