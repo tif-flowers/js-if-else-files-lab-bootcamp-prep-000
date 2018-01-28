1b8cdef302cf6c1d7c3976f9eb1ebc9d269b4012
Tif Flowers <tif@gentlebull.com>
2018-01-28 23:28:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ if (name === "Susan") {
     console.log("Expected the name to be Susan")
 }
 
-if (name === "Susan") {
+if (height === "74") {
     console.log("The name is correct")
 } else {
     console.log("Expected the name to be Susan")
